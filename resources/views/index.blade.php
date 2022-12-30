27eca6fc048c44a3040632147cbfbd631c5897d4
Melquisedeque <melqueataide@gmail.com>
2022-12-30 00:36:58 -0300

    estilização da página home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,12 +3,37 @@
 @section('title', 'MA eventos')
 
 @section('content')
-<h1>Título</h1>
+
+<div id="search-container" class="col-md-12">
+    <h1>Busque um Evento</h1>
+    <form action="">
+        <input type="text" id="search" name="search" class="form-control" placeholder="Procurar...">
+    </form>
+</div>
+<div id="events-container" class="col-md-12">
+    <h2>Próximos Eventos</h2>
+    <p class="subtitle">Veja os eventos dos próximos dias</p>
+    <div id="cards-container" class="row">
+        @foreach ($eventos as $event)
+            <div class="card col-md-3">
+                <img src="/img/banner.jpg" alt="{{ $event->titulo }}">
+                <div class="card-body">
+                    <p class="card-date">10/09/2023</p>
+                    <h5 class="card-title">{{ $event->titulo }}</h5>
+                    <p class="card-participants">x participantes</p>
+                    <a href="#" class="btn btn-primary">Saber mais</a>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+
+{{-- <h1>Título</h1>
 <img src="\img\banner.jpg" alt="Banner" style=" width: 100%">
 
 @foreach ($eventos as $event)
     <p>{{ $event->titulo }} -- {{ $event->descricao }}</p>
-@endforeach
+@endforeach --}}
 
 {{-- https://www.youtube.com/watch?v=_sayRBbEHN8&list=PLnDvRpP8BnewYKI1n2chQrrR4EYiJKbUG&index=8 --}}
 
